754ae2e2826967e59fb99142236217eee18c43ea
Jason May <jmay@pobox.com>
2012-04-27 10:17:26 -0700

    repaired datify test

--- spec/lib/datify_spec.rb
+++ spec/lib/datify_spec.rb
@@ -2,15 +2,14 @@
 
 require "spec_helper"
 
-describe "csvification" do
-  it "should convert pipes to standard commas" do
-    filename = File.dirname(__FILE__) + "/../data/pipe_data.txt"
-    tmp = Tempfile.new('csvify')
-    results = Masticate.csvify(filename, :output => tmp, :col_sep => '|')
+describe "datification" do
+  it "should transform dates" do
+    filename = File.dirname(__FILE__) + "/../data/datify_input.csv"
+    tmp = Tempfile.new('datify')
+    results = Masticate.datify(filename, :output => tmp, :field => 'timestamp', :format => '%m/%d/%Y %H:%M:%S%p')
     output = File.read(tmp)
     tmp.unlink
-    output.lines.count.should == 5
-    results[:input_count].should == 6
-    results[:output_count].should == 5
+
+    correct_output = File.read(File.dirname(__FILE__) + "/../data/datify_result.csv")
   end
 end
