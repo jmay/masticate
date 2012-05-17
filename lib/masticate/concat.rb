d866803925c7faf4236c5203c13da232db9befda
Jason May <jmay@pobox.com>
2012-05-17 10:44:18 -0700

    correct syntax is tail -n +2

--- lib/masticate/concat.rb
+++ lib/masticate/concat.rb
@@ -15,7 +15,7 @@ class Masticate::Concat #< Masticate::Base
     file1, *rest = @filenames
     system "cat #{file1} #{redirect}"
     rest.each do |file|
-      system "tail +2 #{file} #{redirect}"
+      system "tail -n +2 #{file} #{redirect}"
     end
   end
 end
